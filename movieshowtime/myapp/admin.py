268e37526d40cef41fdc85d9216931d3e69c4c31
Olunusib <olunusibest@gmail.com>
2019-12-27 16:28:07 -0500

    Deleted Redundant Files

--- movieshowtime/myapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import SearchByActors,SearchByGenre,SearchByRating,SearchByDirectors,SearchByMovieName
-
-# Register your models here.
-admin.site.register([SearchByActors,SearchByGenre,SearchByRating,SearchByDirectors,SearchByMovieName])
-# Register your models her

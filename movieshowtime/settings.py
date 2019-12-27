9ec22739925a30d08b25a0bdd839b6908b241ae4
Olunusib <olunusibest@gmail.com>
2019-12-27 18:28:19 -0500

    Update Settings

--- movieshowtime/settings.py
+++ movieshowtime/settings.py
@@ -23,7 +23,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -95,7 +94,6 @@ DATABASES = {
     }
 }
 
-print(DATABASES['default']['USER'])
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
